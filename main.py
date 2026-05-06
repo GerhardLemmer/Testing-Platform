846bf29ba8440d9d1e1abbca65a231df243339d2
Gerhard Lemmer <glemmer94@gmail.com>
2026-05-06 11:58:12 +0200

    Name refactor

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from adapters.controllers.payment_controller import router as payment_router
 from adapters.controllers.auth_controller import router as auth_router
 from adapters.controllers.order_controller import router as order_router
-from adapters.controllers.file_upload_cotroller import router as file_upload_router
+from adapters.controllers.file_upload_controller import router as file_upload_router
 
 app = FastAPI()
 
