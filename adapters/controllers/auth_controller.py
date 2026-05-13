fbeed1a325f0a558299c0ed6811f34ba5ddcd1f5
Gerhard Lemmer <glemmer94@gmail.com>
2026-05-13 14:35:29 +0200

    refactored code structure to focus more on end goal needs

--- adapters/controllers/auth_controller.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from application.use_cases.run_scenario import run_auth_scenario
-
-router = APIRouter()
-
-@router.get("/auth")
-def run_scenario(scenario_name: str):
-    result = run_auth_scenario(scenario_name)
-    return result
