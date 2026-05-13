817d75b28aa0d5c6ccd946007772bb20f4f75dfd
GerhardJLemmer <lemmerg@selectafrica.net>
2026-05-13 14:35:29 +0200

    refactored code structure to focus more on end goal needs

--- adapters/controllers/file_upload_controller.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from application.use_cases.run_scenario import run_file_upload_scenario
-
-router = APIRouter()
-
-@router.get("/file-upload")
-def run_scenario(scenario_name: str):
-    result = run_file_upload_scenario(scenario_name)
-    return result
